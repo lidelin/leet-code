545120268ab654196f1c7949a527ca63094cad6a
lidelin <494747693@qq.com>
2019-05-22 22:37:22 +0800

    876. Middle of the Linked List

---
+++ problems/easy/0876-middle-of-the-linked-list/middle-of-the-linked-list.go
@@ -0,0 +1,27 @@
+package _876_middle_of_the_linked_list
+
+import (
+	"leet-code/utils"
+)
+
+type ListNode = utils.ListNode
+
+func MiddleNode(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	slow := head
+	fast := head
+
+	for fast != nil {
+		if fast.Next != nil {
+			fast = fast.Next.Next
+			slow = slow.Next
+		} else {
+			fast = fast.Next
+		}
+	}
+
+	return slow
+}
---
+++ problems/easy/0876-middle-of-the-linked-list/middle-of-the-linked-list_test.go
@@ -0,0 +1,29 @@
+package _876_middle_of_the_linked_list
+
+import (
+	"github.com/stretchr/testify/assert"
+	"leet-code/utils"
+	"testing"
+)
+
+func TestMiddleNode(t *testing.T) {
+	head := utils.MakeList([]int{1, 2, 3, 4, 5})
+	middle := utils.MakeList([]int{3, 4, 5})
+	result := MiddleNode(head)
+	assert.Equal(t, result, middle)
+
+	head = utils.MakeList([]int{1, 2, 3, 4, 5, 6})
+	middle = utils.MakeList([]int{4, 5, 6})
+	result = MiddleNode(head)
+	assert.Equal(t, result, middle)
+
+	head = utils.MakeList([]int{1, 2})
+	middle = utils.MakeList([]int{2})
+	result = MiddleNode(head)
+	assert.Equal(t, result, middle)
+
+	head = utils.MakeList([]int{1})
+	middle = utils.MakeList([]int{1})
+	result = MiddleNode(head)
+	assert.Equal(t, result, middle)
+}
