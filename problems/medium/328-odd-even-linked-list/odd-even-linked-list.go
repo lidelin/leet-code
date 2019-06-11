6a49dfc38ec5f5d729aeba0d279763da296b39d8
lidelin <494747693@qq.com>
2019-06-11 23:10:49 +0800

    328. Odd Even Linked List

---
+++ problems/medium/328-odd-even-linked-list/odd-even-linked-list.go
@@ -0,0 +1,34 @@
+package _28_odd_even_linked_list
+
+import "leet-code/utils"
+
+type ListNode = utils.ListNode
+
+func OddEvenList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	odd := head
+	even, evenHead := head.Next, head.Next
+	current := head.Next.Next
+	count := 1
+
+	for current != nil {
+		if count%2 == 0 {
+			even.Next = current
+			even = even.Next
+		} else {
+			odd.Next = current
+			odd = odd.Next
+		}
+
+		count++
+		current = current.Next
+	}
+
+	even.Next = nil
+	odd.Next = evenHead
+
+	return head
+}
---
+++ problems/medium/328-odd-even-linked-list/odd-even-linked-list_test.go
@@ -0,0 +1,19 @@
+package _28_odd_even_linked_list
+
+import (
+	"github.com/stretchr/testify/assert"
+	"leet-code/utils"
+	"testing"
+)
+
+func TestOddEvenList(t *testing.T) {
+	head := utils.MakeList([]int{1, 2, 3, 4, 5})
+	expected := utils.MakeList([]int{1, 3, 5, 2, 4})
+	actual := OddEvenList(head)
+	assert.Equal(t, expected, actual)
+
+	head = utils.MakeList([]int{2, 1, 3, 5, 6, 4, 7})
+	expected = utils.MakeList([]int{2, 3, 6, 7, 1, 5, 4})
+	actual = OddEvenList(head)
+	assert.Equal(t, expected, actual)
+}
